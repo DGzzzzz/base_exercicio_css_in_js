16c215dc4cff23dc478e6f4c2269b70a352ee2b7
DGzzzzz <douglaswinter75@gmail.com>
2024-04-27 18:19:41 -0300

    conversão para styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import Header from './components/Cabecalho'
+import Cabecalho from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import { Container } from './styles'
+import EstiloGlobal from './styles'
 
 function App() {
   return (
     <>
-      <Header />
+      <EstiloGlobal />
+      <Cabecalho />
       <Hero />
-      <div className="container">
+      <Container className="container">
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
